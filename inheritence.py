85e0f016e54dd7eedaab36db498c448459ba3507
Hari Haran <earlharry6@gmail.com>
2026-02-17 12:33:06 +0530

    new

---
+++ inheritence.py
@@ -0,0 +1,46 @@
+class School:
+    def __init__(self):
+        self.name = input("Enter your name: ")
+        self.SSLC_marks = int(input("Enter your SSLC marks: "))
+        self.HSC_marks = int(input("Enter your HSC marks: "))
+        self.HSC = self.group()
+
+    def group(self):
+        if self.SSLC_marks >= 500:
+            return "Bio-Maths"
+        elif self.SSLC_marks >= 450:
+            return "Computer Science"
+        elif self.SSLC_marks >= 300:
+            return "Commerce"
+        else:
+            return "Arts"
+
+
+class College(School):
+    def college(self):
+        if (self.HSC == "Bio-Maths" or self.HSC == "Computer Science") and self.HSC_marks >= 500:
+            return "Congratulations! You are eligible for all Medical and Engineering colleges VIT, SRM, Anna University, and Madras University"
+
+        elif (self.HSC == "Commerce" or self.HSC == "Arts") and self.HSC_marks >= 450:
+            return "Congratulations! You are eligible for Arts and Science colleges sathyabama and stella maris"
+
+        elif (self.HSC == "Commerce" or self.HSC == "Arts") and self.HSC_marks >= 300:
+            return "Congratulations! You are eligible for Arts Stream only thiyagarajar college and Loyola college"
+
+        else:
+            return "Sorry! You are not eligible for any college based on your marks and group."
+
+    def display(self):
+        print("\n--- Student Details ---")
+        print("Name:", self.name)
+        print("SSLC Marks:", self.SSLC_marks)
+        print("HSC Marks:", self.HSC_marks)
+        print("Group:", self.HSC)
+        print("College:", self.college())
+
+
+# Create object
+deepak = College()
+
+# Show details
+deepak.display()
