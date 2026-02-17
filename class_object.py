597b6f608b97a199eb095749a8ce3fcbae02fc35
Hari Haran <earlharry6@gmail.com>
2026-02-17 11:22:48 +0530

    new

---
+++ class_object.py
@@ -0,0 +1,18 @@
+class StudentData:
+    def __init__(self):
+        self.name = input("Enter name: ")
+        self.age = int(input("Enter age: "))
+        self.roll_no = int(input("Enter roll number: "))
+
+    def display(self):
+        print("\n--- Student Details ---")
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Roll No:", self.roll_no)
+        print("Thanks for submitting your data,", self.name)
+
+# Create object
+student = StudentData()
+
+# Call method
+student.display()
